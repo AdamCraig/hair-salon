1a3aff0c39b3c51a6980dcab76e405893fba5c30
Adam Craig <lateralusajc@gmail.com>
2016-05-06 08:35:26 -0700

    set up databases hair_salon and hair_salon_test, initialize DatabaseRule.java for clearing database between each test

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 public class ClientTest {
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @Test
   public void client_instantiatesCorrectly_true() {
     Client myClient = new Client("Jane");
