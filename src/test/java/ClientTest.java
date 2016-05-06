ac0848468e6bc2a7f778b1d7dc2702d0bf8f2852
Adam Craig <lateralusajc@gmail.com>
2016-05-06 08:23:13 -0700

    add passing unit testing for Client objects instantiating correctly and getName() method correctly returning the name property

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -9,4 +9,10 @@ public class ClientTest {
     Client myClient = new Client("Jane");
     assertEquals(true, myClient instanceof Client);
   }
+
+  @Test
+  public void getName_clientInstantiatesWithName_String() {
+    Client myClient = new Client("Jane");
+    assertEquals("Jane", myClient.getName());
+  }
 }
